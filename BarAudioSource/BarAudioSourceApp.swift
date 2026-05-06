5ce1fa75223d77aa778a4f4067bddb03d1ca8699
Nils Westhoff <nils.westhoff@source.ag>
2026-05-06 08:48:26 +0200

    Initial Commit

---
+++ BarAudioSource/BarAudioSourceApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarAudioSourceApp.swift
+//  BarAudioSource
+//
+//  Created by Nils Westhoff on 06/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct BarAudioSourceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
